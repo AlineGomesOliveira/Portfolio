54151346768b7075c9238785fbb9a34666b4b6a7
Aline Gomes <86137327+AlineGomesOliveira@users.noreply.github.com>
2025-06-17 17:35:59 -0400

    Update 4_👅_Idiomas.py

--- pages/4_👅_Idiomas.py
+++ pages/4_👅_Idiomas.py
@@ -1,45 +1,40 @@
-import streamlit as st
-import streamlit.components.v1 as components
-from PIL import Image
+importar iluminado por fluxo como rua
+importar iluminado por fluxo.componentes.v1 como componentes
+de PIL importar Imagem
 
-st.set_page_config(
-    page_title="Idiomas",
-    page_icon="👅",
+rua.definir_configuração_da_página(
+    título_da_página="Idiomas",
+    ícone_da_página="🌎",
 )
 
+com rua.recipiente():
+    col1, col2, col3 = rua.colunas(3)
 
-with st.container():
-    
-    col1, col2, col3 = st.columns(3)
+    com col1:
+        rua.redução_de_preço("")
 
-    with col1:
-        
-        st.markdown("")
+    com col2:
+        rua.redução_de_preço("")
 
-    with col2:
-        image = Image.open('Bandeira_do_USA.png')
-        st.image(image,width=250)
-    
-    with col3:
-        
-        st.markdown("")
+    com col3:
+        rua.redução_de_preço("")
 
-st.markdown("###### Em 2007 decidi melhorar minha proficiência no Inglês e me mudei para Toronto no Canadá, onde fiquei até 2008 estudando esse idioma e fazendo alguns trabalhos na área da construção para custear minha estadia por lá.")
-st.markdown("###### Entre 2016 a 2018 voltei a morar no Canadá, dessa vez em Montreal e aproveitei para aprender o idioma Francês. Também tive a oportunidade de trabalhar na WADA(World Anti Doping Agency) fazendo serviços administrativos, onde coloquei em prática o que aprendi sobre Inglês e Francês.")
+    rua.redução_de_preço("###### Em 2007 decidi melhorar minha proficiência no Inglês e me mudei para Toronto no Canadá, onde fiquei até 2008 estudando esse idioma e fazendo alguns trabalhos na área da construção para custear minha estadia por lá.")
+    rua.redução_de_preço("###### Entre 2016 a 2018 voltei a morar no Canadá, dessa vez em Montreal e aproveitei para aprender o idioma Francês. Também tive a oportunidade de trabalhar na WADA(World Anti Doping Agency) fazendo serviços administrativos, onde coloquei em prática o que aprendi sobre Inglês e Francês.")
+    rua.redução_de_preço("###### E por falar em Canadá, em nenhuma dessas vezes encontrei a Luísa!!")
 
-st.markdown("###### E por falar em Canadá, em nenhuma destas vezes encontrei a Luisa!!")
+    col4, col5 = rua.colunas(2)
 
+    com col4:
+        imagem_en = Imagem.abrir('Bandeira_do_USA.png')
+        rua.imagem(imagem_en, imagem_largura=200)
+        rua.controle_deslizante("Inglês", 0, 100, 30)
 
-col4, col5 = st.columns(2)
+    com col5:
+        imagem_fr = Imagem.abrir('Bandeira_do_SPAN.png')
+        rua.imagem(imagem_fr, imagem_largura=200)
+        rua.controle_deslizante("Francês", 0, 100, 70)
 
-with col4:
-    
-   st.slider("Inglês",0,100, 90)
-
-
-with col5:
-   st.slider('Francês', 0, 100, 50)
-    
     
 
 
