2c53a19b80cfbc9dbb143a2089696fcbebe4fb4a
Aline Gomes <86137327+AlineGomesOliveira@users.noreply.github.com>
2025-06-17 17:39:44 -0400

    Update 4_👅_Idiomas.py

--- pages/4_👅_Idiomas.py
+++ pages/4_👅_Idiomas.py
@@ -1,42 +1,30 @@
 import streamlit as st
-import streamlit.components.v1 as components
 from PIL import Image
 
+# Page config
 st.set_page_config(
-    page_title="Idiomas",
-    page_icon=="🌎",
+    page_title="Languages",
+    page_icon="🌍",
 )
 
-com rua.recipiente():
-    col1, col2, col3 = rua.colunas(3)
+# Introductory text
+st.markdown("""
+### In 2007 I decided to improve my English proficiency and moved to Toronto, Canada, where I stayed until 2008 studying the language and doing some construction work to pay for my stay there.
 
-    com col1:
-        rua.redução_de_preço("")
+### Between 2016 and 2018 I returned to live in Canada, this time in Montreal, and took the opportunity to learn French. I also had the opportunity to work at WADA (World Anti Doping Agency) doing administrative services, where I put into practice what I had learned about English and French.
 
-    com col2:
-        rua.redução_de_preço("")
+### And speaking of Canada, in none of these times did I ever meet Luisa!!
+""")
 
-    com col3:
-        rua.redução_de_preço("")
+# Two columns: English and French
+col1, col2 = st.columns(2)
 
-    rua.redução_de_preço("###### Em 2007 decidi melhorar minha proficiência no Inglês e me mudei para Toronto no Canadá, onde fiquei até 2008 estudando esse idioma e fazendo alguns trabalhos na área da construção para custear minha estadia por lá.")
-    rua.redução_de_preço("###### Entre 2016 a 2018 voltei a morar no Canadá, dessa vez em Montreal e aproveitei para aprender o idioma Francês. Também tive a oportunidade de trabalhar na WADA(World Anti Doping Agency) fazendo serviços administrativos, onde coloquei em prática o que aprendi sobre Inglês e Francês.")
-    rua.redução_de_preço("###### E por falar em Canadá, em nenhuma dessas vezes encontrei a Luísa!!")
+with col1:
+    image_en = Image.open("Bandeira_do_USA.png")
+    st.image(image_en, width=200)
+    st.slider("English", 0, 100, 30)
 
-    col4, col5 = rua.colunas(2)
-
-    com col4:
-        imagem_en = Imagem.abrir('Bandeira_do_USA.png')
-        rua.imagem(imagem_en, imagem_largura=200)
-        rua.controle_deslizante("Inglês", 0, 100, 30)
-
-    com col5:
-        imagem_fr = Imagem.abrir('Bandeira_do_SPAN.png')
-        rua.imagem(imagem_fr, imagem_largura=200)
-        rua.controle_deslizante("Francês", 0, 100, 70)
-
-    
-
-
-
-
+with col2:
+    image_fr = Image.open("Bandeira_do_USA.png")  # use the name you saved for the second image
+    st.image(image_fr, width=200)
+    st.slider("French", 0, 100, 70)
