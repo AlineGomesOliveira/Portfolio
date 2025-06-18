8ef9cf8c75c0a0557af6fdb1512c88ed4cd96049
Aline Gomes <86137327+AlineGomesOliveira@users.noreply.github.com>
2025-06-18 16:46:53 -0400

    Update 3_⚛️_Projetos_em_Data_Science.py

--- pages/3_⚛️_Projetos_em_Data_Science.py
+++ pages/3_⚛️_Projetos_em_Data_Science.py
@@ -29,7 +29,7 @@ with st.container():
 
     st.markdown("**Resumo:** " )
     st.markdown("> 📌 Neste projeto existiram 2 objetivos principais:")
-    st.markdown("> O 1º objetivo foi aplicar conceitos de Ciência de Dados para analisar um dataset com avaliações de filmes feitas por usuários (MovieLens dataset), investigando padrões de nota com relação a gênero, ano de lançamento e número de avaliações.")
+    st.markdown("> O 1º objetivo foi aplicar conhecimento em Data Science e responder 8 perguntas sobre Logística e Consumo de Combustível com tabelas e gráficos gerados com os pacotes Pandas, Matplotlib e Seaborn do Python.")
     st.markdown("> 0 2º objetivo foi criar modelos de Machine Learning para Prever de Consumo de Combustível. Para esta previsão foi usado um modelo de Regressão(Benchmark), um modelo SVM e um modelo SVM com otimização de parâmetros através da biblioteca Python SciKit-learn.")
     st.markdown("> As métricas usadas foram Coeficiente R2 e RMSE.") 
     st.markdown("> O objetivo foi alcançar um Coeficiente R2 de 70%.")
