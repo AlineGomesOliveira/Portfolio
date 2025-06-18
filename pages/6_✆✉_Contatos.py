11211f0346205912a857413a6f0392770c3f3814
Aline Gomes <86137327+AlineGomesOliveira@users.noreply.github.com>
2025-06-18 11:14:44 -0400

    Update 6_✆✉_Contatos.py

--- pages/6_✆✉_Contatos.py
+++ pages/6_✆✉_Contatos.py
@@ -13,7 +13,7 @@ components.html("""<script src="https://platform.linkedin.com/badges/js/profile.
 <div class="badge-base LI-profile-badge" data-locale="en_US" data-size="medium" data-theme="light" data-type="VERTICAL" data-vanity="evandro-cleto-51b49340" data-version="v1"><a class="badge-base__link LI-simple-link" href="hhttps://www.linkedin.com/in/aline-gomes-563408169/?trk=profile-badge"></a></div>""",height=250)
     
 
-st.write('📧: **evandrocleto74@gmail.com**')
-#st.write('📱: **+551197069-0730**')
-st.write('LinkedIn: https://www.linkedin.com/in/evandro-cleto/?locale=pt_BR')
-st.write('GitHub:https://github.com/EvandroCleto')
+st.write('📧: **linegomes1509@gmail.com**')
+#st.write('📱: **+559299502-1056**')
+st.write('LinkedIn: https://www.linkedin.com/in/aline-gomes-563408169/?locale=pt_BR')
+st.write('GitHub: https://github.com/AlineGomesOliveira')
