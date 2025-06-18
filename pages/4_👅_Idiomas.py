2c2e7ff4ebb67221c569aa629a05a9aa1d22f091
Aline Gomes <86137327+AlineGomesOliveira@users.noreply.github.com>
2025-06-18 09:09:58 -0400

    Update 4_👅_Idiomas.py

--- pages/4_👅_Idiomas.py
+++ pages/4_👅_Idiomas.py
@@ -1,30 +1,41 @@
 import streamlit as st
+import streamlit.components.v1 as components
 from PIL import Image
 
-# Page config
 st.set_page_config(
-    page_title="Languages",
-    page_icon="🌍",
+    page_title="Idiomas",
+    page_icon="👅",
 )
 
-# Introductory text
-col1 = st.columns(1)
 
-with col1:
-    st.markdown('<div style="font-size: 14px">Olá! Eu sou Aline Gomes!!</div>', unsafe_allow_html=True)
+with st.container():
+    
+    col1, col2, col3 = st.columns(3)
+
+    with col1:
+        
+        st.markdown("")
+
+    with col2:
+        image = Image.open('Bandeira_do_USA.png')
+        st.image(image,width=250)
+    
+    with col3:
+        
+        st.markdown("")
+
+st.markdown("###### Em 2007 decidi melhorar minha proficiência no Inglês e me mudei para Toronto no Canadá, onde fiquei até 2008 estudando esse idioma e fazendo alguns trabalhos na área da construção para custear minha estadia por lá.")
+st.markdown("###### Entre 2016 a 2018 voltei a morar no Canadá, dessa vez em Montreal e aproveitei para aprender o idioma Francês. Também tive a oportunidade de trabalhar na WADA(World Anti Doping Agency) fazendo serviços administrativos, onde coloquei em prática o que aprendi sobre Inglês e Francês.")
+
+st.markdown("###### E por falar em Canadá, em nenhuma destas vezes encontrei a Luisa!!")
 
 
-  st.text("")
+col4, col5 = st.columns(2)
 
-# Two columns: English and French
-col2, col3 = st.columns(2)
+with col4:
+    
+   st.slider("Inglês",0,100, 90)
 
-with col2:
-    image_en = Image.open("Bandeira_do_USA.png")
-    st.image(image_en, width=200)
-    st.slider("English", 0, 100, 30)
 
-with col3:
-    image_fr = Image.open("Bandeira_do_SPAN.png")  # use the name you saved for the second image
-    st.image(image_fr, width=200)
-    st.slider("French", 0, 100, 70)
+with col5:
+   st.slider('Francês', 0, 100, 50)
