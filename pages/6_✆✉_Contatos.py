17724d3726c8fb8a34f164e8effa1096bb8e2efd
Aline Gomes <86137327+AlineGomesOliveira@users.noreply.github.com>
2025-06-18 11:27:45 -0400

    Update 6_✆✉_Contatos.py

--- pages/6_✆✉_Contatos.py
+++ pages/6_✆✉_Contatos.py
@@ -11,7 +11,24 @@ st.write('### **Como me encontrar:**')
 
 components.html("""<script src="https://platform.linkedin.com/badges/js/profile.js" async defer type="text/javascript"></script>
 <div class="badge-base LI-profile-badge" data-locale="en_US" data-size="medium" data-theme="light" data-type="VERTICAL" data-vanity="aaline-gomes-563408169" data-version="v1"><a class="badge-base__link LI-simple-link" href="hhttps://www.linkedin.com/in/aline-gomes-563408169/?trk=profile-badge"></a></div>""",height=250)
-    
+
+# Badge LinkedIn
+componentes.HTML("""
+  <!-- LinkedIn Profile Badge -->
+  <script src="https://platform.linkedin.com/badges/js/profile.js" async defer type="text/javascript"></script>
+  <div class="badge-base LI-profile-badge"
+       data-locale="pt_BR"
+       data-size="medium"
+       data-theme="light"
+       data-type="VERTICAL"
+       data-vanity="SEU-VANITY"
+       data-version="v1">
+    <a class="badge-base__link LI-simple-link"
+       href="https://www.linkedin.com/in/SEU-VANITY">
+       Aline Gomes no LinkedIn
+    </a>
+  </div>
+""", height=300)
 
 
 st.write('📧:  **linegomes1509@gmail.com**')
