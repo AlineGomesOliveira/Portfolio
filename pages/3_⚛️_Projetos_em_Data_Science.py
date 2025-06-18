d3afa2100e17c39171b375600610a5341340b6fa
Aline Gomes <86137327+AlineGomesOliveira@users.noreply.github.com>
2025-06-18 16:49:52 -0400

    Update 3_⚛️_Projetos_em_Data_Science.py

--- pages/3_⚛️_Projetos_em_Data_Science.py
+++ pages/3_⚛️_Projetos_em_Data_Science.py
@@ -29,7 +29,7 @@ with st.container():
     col15 = st.columns(1)
 
     st.markdown("**Resumo:** " )
-    st.markdown("> Neste projeto existiram 2 objetivos:")
+    st.markdown("> Neste projeto existiram 2 objetivos principais:")
     st.markdown("> O 1º objetivo foi aplicar conhecimento em Data Science e responder 8 perguntas sobre Logística e Consumo de Combustível com tabelas e gráficos gerados com os pacotes Pandas, Matplotlib e Seaborn do Python.")
     st.markdown("> 0 2º objetivo foi criar modelos de Machine Learning para Prever de Consumo de Combustível. Para esta previsão foi usado um modelo de Regressão(Benchmark), um modelo SVM e um modelo SVM com otimização de parâmetros através da biblioteca Python SciKit-learn.")
     st.markdown("> As métricas usadas foram Coeficiente R2 e RMSE.") 
