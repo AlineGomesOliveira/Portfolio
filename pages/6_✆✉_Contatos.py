a4439b094fa484df8ce76d4770d7cfa0afc50e79
Aline Gomes <86137327+AlineGomesOliveira@users.noreply.github.com>
2025-06-18 11:28:23 -0400

    Update 6_✆✉_Contatos.py

--- pages/6_✆✉_Contatos.py
+++ pages/6_✆✉_Contatos.py
@@ -12,6 +12,24 @@ st.write('### **Como me encontrar:**')
 components.html("""<script src="https://platform.linkedin.com/badges/js/profile.js" async defer type="text/javascript"></script>
 <div class="badge-base LI-profile-badge" data-locale="en_US" data-size="medium" data-theme="light" data-type="VERTICAL" data-vanity="aaline-gomes-563408169" data-version="v1"><a class="badge-base__link LI-simple-link" href="hhttps://www.linkedin.com/in/aline-gomes-563408169/?trk=profile-badge"></a></div>""",height=250)
 
+
+
+st.write('📧:  **linegomes1509@gmail.com**')
+#st.write('📱: **+559299502-1056**')
+st.write('LinkedIn:  https://www.linkedin.com/in/aline-gomes-563408169/?locale=pt_BR')
+st.write('GitHub:  https://github.com/AlineGomesOliveira')
+
+import iluminado as rua
+from PIL import Image
+import iluminado.fluxo.componentes.v1 as componentes
+
+rua.definir_configuração_da_página(
+    título_da_página="Contatos",
+    ícone_de_página="☎️",
+)
+
+rua.escrever("## Como me encontrar:\n")
+
 # Badge LinkedIn
 componentes.HTML("""
   <!-- LinkedIn Profile Badge -->
@@ -30,8 +48,5 @@ componentes.HTML("""
   </div>
 """, height=300)
 
-
-st.write('📧:  **linegomes1509@gmail.com**')
-#st.write('📱: **+559299502-1056**')
-st.write('LinkedIn:  https://www.linkedin.com/in/aline-gomes-563408169/?locale=pt_BR')
-st.write('GitHub:  https://github.com/AlineGomesOliveira')
+rua.escrever("✉️: alinegomes1509@gmail.com\n\n")
+rua.escrever("GitHub: https://github.com/AlineGomesOliveira")
